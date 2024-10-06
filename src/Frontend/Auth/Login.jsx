07d25d097644caa28b503138b465f284fb59def7
Manoj Naik <manoj.naik.4work@gmail.com>
2024-10-06 23:11:43 +0530

    login ui pending

--- src/Frontend/Auth/Login.jsx
+++ src/Frontend/Auth/Login.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 
-function Login() {
+const LoginForm = () => {
   return (
-    <>
-      
-    </>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-2xl w-96">
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-sm font-medium text-gray-700 mb-1"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-sm font-medium text-gray-700 mb-1"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-gray-700 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
   );
-}
+};
 
-export default Login;
+export default LoginForm;
